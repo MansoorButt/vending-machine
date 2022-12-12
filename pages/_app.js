9f48b8e3429e1cafc94d11f662c2c217aadbb8b7
MansoorButt <balajmen@gmail.com>
2022-12-12 22:59:30 +0500

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from "@ethersproject/providers"
+import { InjectedConnector } from '@web3-react/injected-connector'
+import Home from '.';
+
+const getLibrary = (provider) =>{
+  return new Web3Provider(provider);
+}
+
+const MetaMask = new InjectedConnector({ supportedNetworks: [5] });
+const connectors = { MetaMask };
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary} connectors={connectors}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+    )
 }
 
 export default MyApp
